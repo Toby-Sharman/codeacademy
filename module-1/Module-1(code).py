f0d8d0b7404221430105512363fb1445d5177189
Toby-Sharman <37976673+Toby-Sharman@users.noreply.github.com>
2018-04-01 17:18:17 +0100

    Merge branch 'master' of https://github.com/Toby-Sharman/codeacademy

---
+++ module-1/Module-1(code).py
@@ -0,0 +1,63 @@
+# Exercise 1
+print "Hello World"
+# Exercise 2
+   # Version 2
+print "Hello World"
+
+   # Version 3
+print ("Hello World")
+# Exercise 3
+print "Hello " + "World"
+# Exercise 4
+   #n/a
+# Exercise 5
+Hello World = 22
+# Exercise 6
+addition = 12381 + 91817
+subtraction = 981 - 312
+multiplication = 38 * 902
+division = 540 / 45
+combinations = 129 * 1345 + 120 / 6 - 12
+division_with_remainder = 7 % 2
+# Exercise 7
+money_in_wallet = 40
+sandwich_price = 7.50
+sales_tax = .08 * sandwich_price
+
+sandwich_price += sales_tax
+money_in_wallet -= sandwich_price
+# Exercise 8
+#Hello World
+# Exercise 9
+int1 = 1
+int2 = 10
+int3 = -5
+
+float1 = 1.0
+float2 = 10.
+float3 = -5.5
+
+float4 = 1.5e2 = 1.5 * 10 ** 2
+# Exercise 10
+integer_remaining_after_division = 50 % 3
+# Exercise 11
+"""Hello
+Hello
+Hello
+"""
+# Exercise 12
+a = True
+b = False
+# Exercise 13
+age = 13
+print "I am " + str(age) + " years old!"
+
+number1 = "100"
+number2 = "10"
+string_addition = number1 + number2
+int_addition = int(number1) + int(number2)
+
+print int(string_num)
+print float(string_num)
+# Exercise 14
+n/a
